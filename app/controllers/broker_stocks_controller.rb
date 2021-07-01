fb2bcb06e24700406f3be9b7b2155f73c0dcd0ae
Leif Bersamina <leifbersamina@gmail.com>
2021-07-01 13:42:16 +0800

    add search feature for stocks

--- app/controllers/broker_stocks_controller.rb
+++ app/controllers/broker_stocks_controller.rb
@@ -3,21 +3,21 @@ class BrokerStocksController < ApplicationController
   before_action :authenticate_broker!, except: %i[index show]
   def index
     if broker_signed_in?
-      @broker_stocks = current_broker.broker_stocks.all
+      @broker_stocks = current_broker.broker_stocks.order(created_at: :desc)
     elsif buyer_signed_in?
       @available_stocks = BrokerStock.all
     end
   end
 
   def new
-    @stocks = Stock.all
+    @stocks = Stock.search(params[:search])
     @broker_stock = current_broker.broker_stocks.build
   end
 
   def show
     @broker_stock = BrokerStock.find(params[:id])
     @client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.dig(:iex_cloud, :api_key),
+      publishable_token: Rails.application.credentials.iex_cloud[:api_key],
       secret_token: Rails.application.credentials.dig(:iex_cloud, :secret_key),
       endpoint: 'https://cloud.iexapis.com/v1'
     )
@@ -28,10 +28,10 @@ class BrokerStocksController < ApplicationController
     @broker_stock = current_broker.broker_stocks.build(broker_stock_params)
 
     if @broker_stock.save
-      redirect_to broker_stocks_path, notice: 'Successfully add stocks'
+      redirect_to new_broker_stock_path, notice: 'Successfully add stocks'
 
     else
-      redirect_to :new, alert: @broker_stocks.errors.full_messages
+      redirect_to new_broker_stock_path, alert: @broker_stocks.errors.full_messages
     end
   end
 
@@ -39,5 +39,9 @@ class BrokerStocksController < ApplicationController
 
   def broker_stock_params
     params.require(:broker_stock).permit(:symbol, :company_name)
+  end
+
+  def search_params
+    params.permit(:search, :commit)
   end
 end
