7834fd02be189e4f8b6562443659e1e0019e62d7
Leif Bersamina <leifbersamina@gmail.com>
2021-07-02 18:08:49 +0800

    enable the index to show the current user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
-  def index; end
+  def index
+    if buyer_signed_in?
+      @user = current_buyer
+    elsif broker_signed_in?
+      @user = current_broker
+    end
+  end
 end
